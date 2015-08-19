db950ced83a1cb7f3161b862023d9fc67a2c55da
Zhicheng Yan <zyan3@zyan3-2.cs.illinois.edu>
2015-08-19 16:26:03 -0500

    1) code cleaning 2) add scripts for training ReNet RNNs on Stanford Background

---
+++ examples/stanford_background/train_renet_2_lay.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CHECKPOINT_DIR=./examples/stanford_background/renet_2_lay
+
+if [ ! -d "$CHECKPOINT_DIR" ]; then
+	mkdir $CHECKPOINT_DIR
+fi
+
+GLOG_logtostderr=1 \
+# GLOG_minloglevel=1 \
+./build/tools/caffe train \
+--solver=./examples/stanford_background/renet_2_lay_HP_nolbw_nopeep_solver.prototxt
